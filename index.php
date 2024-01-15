c129468247bc1eef6b69bc1f1b1792972ceb03f5
Kasperdejong <kasperdejong@gmail.com>
2024-01-15 13:11:17 +0100

    index.php en css
    beginnetje gemaakt aan nav bar en header. nav bijna klaar. css ook mee gekloot

---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Home</title>
+</head>
+<body>
+
+    <nav>
+        <img class="icon-image" src="includes/images/peitsman-licht-geluid.png">
+        <a href="#producten-nav">Producten</a>
+        <a href="#home-nav">Home</a>
+        <a href="#reserveer-nav">Reserveer</a>
+        <a href="#contact-nav">Contact</a>
+        <a href="#log-in-nav">Log in</a>
+        <img class="profile-icon" src="includes/images/icon.png">
+    </nav>
+
+     <div class="line"></div>
+     
+    <div class="center-container">
+
+
+        <header>
+
+            <h1>Welkom bij Peitsman</h1>
+
+                <p>
+                    Wij zijn Peitsman, gevestigd in Rotterdam, Utrecht en Zwolle.
+                    Wij houden van beeld, licht en geluid en we willen graag laten zien welke fantastische dingen
+                    je ermee kunt doen. Wij denken mee en bieden creatieve oplossingen op het gebied van
+                    audiovisuele techniek. Voor organisatoren en producenten van (bedrijfs)evenementen
+                    maar ook in vaste installaties en theatertours.
+                </p>
+
+        </header>
+
+        <main id="producten">
+
+            <h2>Onze producten</h2>
+
+            <div id="cards">
+
+                <div class="card-contents">
+                    <div class="card-contents-img"><img src="Images/bas-versterker.jpg"></div>
+                    <div class="card-contents-txt">
+                        <h3>Versterker</h3>
+                            <div class="rating">
+                                <img src="Images/yellow-star.jpeg">
+                                <img src="Images/yellow-star.jpeg">
+                                <img src="Images/yellow-star.jpeg">
+                                <img src="Images/yellow-star.jpeg">
+                                <img src="Images/grey-star.jpeg">
+                            </div>
+                            <p>Deze versterker werkt goed bij de bas, anders komt er geen geluid uit.</p>
+                            <button>
+                                <a href="#https://www.bax-shop.nl/basgitaarversterker-combo/hartke-hd25-basversterker"
+                                    target="_blank_">Bestellen</a>
+                            </button>
+                    </div>
+                 </div>
+
+                <div class="card-contents">
+                    <div class="card-contents-img"><img src="Images/bass-strings.png"></div>
+                    <div class="card-contents-txt">
+                        <h3>Bas strings</h3>
+                            <div class="rating">
+                                <img src="Images/yellow-star.jpeg">
+                                <img src="Images/yellow-star.jpeg">
+                                <img src="Images/grey-star.jpeg">
+                                <img src="Images/grey-star.jpeg">
+                                <img src="Images/grey-star.jpeg">
+                            </div>
+                            <p>Welke bas kan gebruikt worden zonder strings? Koop nu!</p>
+                            <button>
+                                <a href="#https://www.bax-shop.nl/elektrische-bassnaren/ernie-ball-3834-coated-bass-super-slinky-45-100-snarenset/product-details.html"
+                                    target="_blank_">Bestellen</a>
+                            </button>
+                     </div>
+                </div>
+
+                    <div class="card-contents">
+                        <div class="card-contents-img"><img src="Images/bas-stand.jpg"></div>
+                        <div class="card-contents-txt">
+                            <h3>Bas stand</h3>
+                            <div class="rating">
+                                <img src="Images/yellow-star.jpeg">
+                                <img src="Images/yellow-star.jpeg">
+                                <img src="Images/yellow-star.jpeg">
+                                <img src="Images/grey-star.jpeg">
+                                <img src="Images/grey-star.jpeg">
+                            </div>
+                            <p>Voor als je de bas even weg wilt leggen is dit de ideale oplossing.</p>
+
+                            <button>
+                                <a href="#https://www.bax-shop.nl/gitaar-statief/fender-adjustable-guitar-stand-standaard-voor-elektrische-en-akoestische-bas-gitaar"
+                                    target="_blank_">Bestellen</a>
+                            </button>
+                        </div>
+                    </div>
+
+                </div>
+
+            </main>
+    </div>
+
+</body>
+</html>
