fc1d03855321b9cc1522a942dad1f51205ba2922
Quinten <quinblom321@gmail.com>
2024-01-15 13:02:10 +0100

    Functions in Classes Done.
    Alle functies die nodig zijn voor een CRUD zitten in de classes. Ik heb commentaar gegeven waar nodig is.

--- includes/classes/Categories/Category.php
+++ includes/classes/Categories/Category.php
@@ -7,4 +7,14 @@ class Category
     public int $id;
     public int $type_number;
     public string $name;
+
+    /**
+     * Omdat er niet meer dan 3 categorien van producten zijn, maken wij hier geen CRUD mogelijkheden voor aan.
+     * Wij roepen alleen de categorien aan voor specifieke producten.
+     **/
+    public static function getAll(\PDO $db): array
+    {
+        return $db->query('SELECT * 
+                                FROM categories;')->fetchAll(\PDO::FETCH_CLASS, '\includes\classes\Categories\Category');
+    }
 }
