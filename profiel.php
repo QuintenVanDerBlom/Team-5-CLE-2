592ac2113c6b17b240e2720d0114c6618814c52a
Kasperdejong <kasperdejong@gmail.com>
2024-01-18 15:13:42 +0100

    profiel.php
    dingen gefixed en connectie met database zou goed moeten gaan als je een actieve session hebt

--- profiel.php
+++ profiel.php
@@ -17,6 +17,9 @@ if(isset($_POST['submit'])) {
     $email = $_POST['email'];
     $number = $_POST['number'];
     $password = $_POST['password'];
+    $adres = $_POST['adres'];
+    $postcode = $_POST['postcode'];
+
 
     // Server-side validation
     if($firstName === '') {
@@ -31,6 +34,10 @@ if(isset($_POST['submit'])) {
         $errors['number'] = $errorMessage;
     }if($password === '') {
         $errors['password'] = $errorMessage;
+    }if($adres === '') {
+        $errors['adres'] = $errorMessage;
+    }if($postcode === '') {
+        $errors['postcode'] = $errorMessage;
     }
 
     // create a secure password, with the PHP function password_hash()
@@ -38,8 +45,8 @@ if(isset($_POST['submit'])) {
         $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
         // store the new user in the database.
-        $query = "INSERT INTO `users`(`id`, `first_name`, `last_name`, `date_of_birth`, `mail_adres`, `phone_number`, `password`) 
-                  VALUES ('','$firstName','$lastName','$dateOfBirth','$email','$number','$hashedPassword')";
+        $query = "INSERT INTO `users`(`id`, `first_name`, `last_name`,`date_of_birth`, `mail_adres`, `phone_number`, `password`, `adres`,`postcode`) 
+                  VALUES ('','$firstName','$lastName','$DoB','$email','$number','$hashedPassword','$adres','$postcode')";
 
         $db->query($query);
     }
@@ -63,76 +70,107 @@ if(isset($_POST['submit'])) {
 
      <div class="line"></div>
 
-    <section id="formContainer">
-        <h2 class="title">Mijn profiel</h2>
-        <form  action="" method="post">
+    <section class="profileFlex">
 
-            <!-- First name -->
-            <section class="formItem" id="firstName-Section">
-                <label for="firstName">First name</label>
-                <input class="form-input" id="firstName" type="text" name="firstName" value=" <?= $firstName ?? ''?>" />
+        <section id="formContainer">
+            <h2 class="title">Mijn profiel</h2>
+            <form  action="" method="post">
 
-                <div>
-                    <p> <?= $errors['firstName'] ?? ''?> </p>
-                </div>
-            </section>
+                <!-- First name -->
+                <section class="formItem" id="firstName-Section">
+                    <label for="firstName">First name</label>
+                    <input class="form-input" id="firstName" type="text" name="firstName" value=" <?= $firstName ?? ''?>" />
 
-            <!-- Last name -->
-            <section class="formItem" id="lastName-Section">
-                <label for="lastName">Last name</label>
-                <input class="form-input" id="lastName" type="text" name="lastName" value="<?= $lastName ?? ''?>" />
+                    <div>
+                        <p> <?= $errors['firstName'] ?? ''?> </p>
+                    </div>
+                </section>
 
-                <div id="errorLastName">
-                    <p> <?= $errors['lastName'] ?? ''?> </p>
-                </div>
-            </section>
+                <!-- Last name -->
+                <section class="formItem" id="lastName-Section">
+                    <label for="lastName">Last name</label>
+                    <input class="form-input" id="lastName" type="text" name="lastName" value="<?= $lastName ?? ''?>" />
 
-            <!-- Date Of Birth -->
-            <section class="formItem" id="DOB-Section">
-                <label for="DOB">Date of Birth</label>
-                <input class="form-input" id="DoB" type="date" name="DoB" value="<?= $DoB ?? ''?>" />
-
-                <div id="errorDoB">
-                    <p> <?= $errors['DoB'] ?? ''?> </p>
-                </div>
-            </section>
-
-            <!-- Email -->
-            <section class="formItem" id="email-Section">
-                <label for="email">Email</label>
-                <input class="form-input" id="email" type="text" name="email" value="<?= $email ?? ''?>" />
-
-                <div id="errorEmail">
-                    <p> <?= $errors['email'] ?? ''?> </p>
-                </div>
-            </section>
-
-            <!-- Number -->
-            <section class="formItem" id="number-Section">
-                <label class="label" for="number">Number</label>
-                <input class="form-input" id="number" type="number" name="number" value="<?= $number ?? ''?>" />
-
-                <div id="errorNumber">
-                    <p class="help is-danger"> <?= $errors['number'] ?? ''?> </p>
-                </div>
-            </section>
+                    <div id="errorLastName">
+                        <p> <?= $errors['lastName'] ?? ''?> </p>
+                    </div>
+                </section>
 
                 <!-- Password -->
-            <section class="formItem" id="password-Section">
-                <label for="password">Password</label>
-                <input class="form-input" id="password" type="password" name="password"/>
+                <section class="formItem" id="password-Section">
+                    <label for="password">Password</label>
+                    <input class="form-input" id="password" type="password" name="password"/>
 
-                <div id="errorPassword">
-                    <p> <?= $errors['password'] ?? ''?> </p>
-                </div>
-            </section>
+                    <div id="errorPassword">
+                        <p> <?= $errors['password'] ?? ''?> </p>
+                    </div>
+                </section>
 
-            <!-- Submit -->
-            <section id="submit">
-                    <button id="submitButton" type="submit" name="submit">Register</button>
-            </section>
+                <!-- Date Of Birth -->
+                <section class="formItem" id="DOB-Section">
+                    <label for="DOB">Date of Birth</label>
+                    <input class="form-input" id="DoB" type="date" name="DoB" value="<?= $DoB ?? ''?>" />
 
-        </form>
+                    <div id="errorDoB">
+                        <p> <?= $errors['DoB'] ?? ''?> </p>
+                    </div>
+                </section>
+
+                <!-- Adres -->
+                <section class="formItem" id="adres-Section">
+                    <label for="adres">Adres</label>
+                    <input class="form-input" id="adres" type="text" name="adres" value="<?= $adres ?? ''?>" />
+
+                    <div id="errorAdres">
+                        <p> <?= $errors['adres'] ?? ''?> </p>
+                    </div>
+                </section>
+
+                <!-- Postcode -->
+                <section class="formItem" id="postcode-Section">
+                    <label for="postcode">Postcode</label>
+                    <input class="form-input" id="postcode" type="text" name="postcode" value="<?= $postcode ?? ''?>" />
+
+                    <div id="errorPostcode">
+                        <p> <?= $errors['postcode'] ?? ''?> </p>
+                    </div>
+                </section>
+
+                <!-- Email -->
+                <section class="formItem" id="email-Section">
+                    <label for="email">Email</label>
+                    <input class="form-input" id="email" type="text" name="email" value="<?= $email ?? ''?>" />
+
+                    <div id="errorEmail">
+                        <p> <?= $errors['email'] ?? ''?> </p>
+                    </div>
+                </section>
+
+                <!-- Number -->
+                <section class="formItem" id="number-Section">
+                    <label class="label" for="number">Number</label>
+                    <input class="form-input" id="number" type="number" name="number" value="<?= $number ?? ''?>" />
+
+                    <div id="errorNumber">
+                        <p class="help is-danger"> <?= $errors['number'] ?? ''?> </p>
+                    </div>
+                </section>
+
+                <!-- Submit -->
+                <section id="submit">
+                        <button id="submitButton" type="submit" name="submit">Wijzigingen opslaan</button>
+                </section>
+
+            </form>
+        </section>
+
+        <section class= "rightSide">
+
+            <img id= profile-icon-big src="includes/images/icon.png">
+
+            <h1>Welkom <?= $firstName?></h1>
+        </section>
+
     </section>
 
 <footer>
